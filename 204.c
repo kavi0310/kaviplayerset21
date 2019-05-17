3f559888f5e5152d21932c8c9aaa443c6809ccf0
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-05-17 12:56:34 +0530

    Create 204.c

---
+++ 204.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        {
+            k=k+a[i];
+        }
+    }
+    printf("%d",k);
+}
